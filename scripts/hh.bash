7af54920d3771e373b2e165679d3832e34bbcc6a
Itay Donenhirsch <itayd@il.ibm.com>
2010-09-30 23:55:36 +0200

    s/!/|/ + ""

--- scripts/hh.bash
+++ scripts/hh.bash
@@ -1,7 +1,7 @@
 h() {
     local rc=0
     local args=$*
-    [[ $args == \! ]] && args=""
+    [[ $args == "|" ]] && args=""
     if hh $args; then
         local what=`cat ~/.hh.last | head -n 1`
         rm -f /tmp/.hh.tmp
